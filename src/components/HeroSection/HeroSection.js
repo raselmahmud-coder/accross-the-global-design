0c3d5ebd14fb6e5e9ca78d8adbfa61975118d313
RASEL MAHMUD <raselmahmud98262@gmail.com>
2022-08-18 05:32:01 +0800

    article post added

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,6 +1,19 @@
 import { Nav } from "react-bootstrap";
 import arrow from "../../assets/arrow-bottom.png";
 import group from "../../assets/group.png";
+import location from "../../assets/location-icon.png";
+import pen from "../../assets/pen-icon.png";
+import profile4 from "../../assets/profile4.png";
+import profile3 from "../../assets/profile3.png";
+import profile2 from "../../assets/profile2.png";
+import cover1 from "../../assets/post-img1.png";
+import cover2 from "../../assets/post-img2.png";
+import cover3 from "../../assets/post-image3.png";
+import article from "../../assets/article.png";
+import education from "../../assets/education-icon.png";
+import alertIcon from "../../assets/alert-icon.png";
+import ArticlePosts from "../Posts/ArticlePosts";
+import JobPosts from "../Posts/JobPosts";
 
 const HeroSection = () => {
   const customStyles = {
@@ -13,7 +26,6 @@ const HeroSection = () => {
     customPadding: {
       padding: "0px 64px",
     },
-
   };
 
   return (
@@ -60,7 +72,7 @@ const HeroSection = () => {
                 <Nav.Link eventKey="link-3">Job</Nav.Link>
               </Nav.Item>
             </Nav>
-                  </div>
+          </div>
           <div className="col-md-4 d-flex justify-content-end">
             <button
               className="btn btn-light me-2"
@@ -74,7 +86,75 @@ const HeroSection = () => {
             </button>
           </div>
         </div>
-                  <hr />
+        <hr className=" mb-4" />
+
+        <div className="row">
+          <div className="col-md-8 pe-5">
+            <ArticlePosts
+              coverImg={cover1}
+              categoryImg={article}
+              title={
+                "What if famous brands had regular fonts? Meet RegulaBrands!"
+              }
+              description={
+                "I’ve worked in UX for the better part of a decade. From now on, I plan to rei…"
+              }
+              profile={profile4}
+              name={"Sarthak Kamra"}
+            />
+            <ArticlePosts
+              coverImg={cover2}
+              categoryImg={education}
+              title={
+                "Tax Benefits for Investment under National Pension Scheme launched by Government"
+              }
+              description={
+                "I’ve worked in UX for the better part of a decade. From now on, I plan to rei…"
+              }
+              profile={profile3}
+              name={"Sarah West"}
+            />
+
+            <JobPosts
+              coverImg={cover3}
+              categoryImg={education}
+              title={
+                "Tax Benefits for Investment under National Pension Scheme launched by Government"
+              }
+              description={
+                "I’ve worked in UX for the better part of a decade. From now on, I plan to rei…"
+              }
+              profile={profile2}
+              name={"Ronal Jones"}
+            />
+          </div>
+          <div className="col-md-4 ps-5">
+            <div
+              className="d-flex justify-content-between"
+              style={{
+                borderBottom: "1px solid gray",
+                marginBottom: "32px",
+              }}>
+              <p>
+                <img
+                  src={location}
+                  alt=""
+                  style={{ width: "20px", height: "20px" }}
+                  className="img-fluid"
+                />
+                Noida, India
+              </p>
+
+              <img
+                src={pen}
+                role="button"
+                style={{ width: "20px", height: "20px" }}
+                alt=""
+              />
+            </div>
+            <img src={alertIcon} alt="" />
+          </div>
+        </div>
       </section>
     </>
   );
---
+++ src/components/Posts/JobPosts.js
@@ -0,0 +1,82 @@
+import eye from "../../assets/eye.png";
+import share from "../../assets/share-icon.png";
+import dot from "../../assets/3dot-icon.png";
+
+const JobPosts = ({
+  coverImg,
+  categoryImg,
+  title,
+  description,
+  profile,
+  name,
+}) => {
+  return (
+    <article
+      style={{
+        background: "#FFFFFF",
+        border: "1px solid #E0E0E0",
+        borderRadius: "4px",
+        marginBottom: "16px",
+      }}>
+      <img src={coverImg} className="img-fluid" alt="" />
+      <div style={{ padding: "20px" }}>
+        <img src={categoryImg} className="img-fluid" alt="" />
+        <div className="d-flex justify-content-between">
+          <h3
+            style={{
+              fontWeight: "1000",
+              fontSize: "22px",
+              lineHeight: "134.17%",
+              padding: "10px 0px",
+            }}>
+            {title}
+          </h3>
+          <img
+            src={dot}
+            role="button"
+            style={{ width: "28px", height: "28px" }}
+            alt=""
+          />
+        </div>
+        <p
+          style={{
+            fontWeight: "400",
+            fontSize: "19px",
+            lineHeight: "134.17%",
+            color: "#5C5C5C",
+          }}>
+          {description}
+        </p>
+        <div className="row row-cols-2">
+          <div className="col d-flex align-items-center">
+            <img src={profile} alt="" role="button" />
+            <h3
+              style={{
+                fontWeight: "600",
+                fontSize: "18px",
+                lineHeight: "23px",
+                marginLeft: "10px",
+              }}>
+              {name}
+            </h3>
+          </div>
+
+          <div className="col">
+            <div className="d-flex justify-content-end align-items-center">
+              <img
+                src={eye}
+                className="img-fluid"
+                style={{ width: "18px", height: "18px", marginRight: "8px" }}
+                alt=""
+              />
+              1.4k views
+              <img src={share} alt="" className="ms-5" role="button" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default JobPosts;
